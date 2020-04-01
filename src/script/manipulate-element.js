a9c01ab1b753244e2b44d52b2904b15197bd2b23
fsy2001 <fsy2001@outlook.com>
2020-04-01 21:17:50 +0800

    nav-bar: add user account pop out when hovered.

---
+++ src/script/manipulate-element.js
@@ -0,0 +1,17 @@
+// bug: when resizing, block may cause problem.
+
+function clickNav() {
+    let nav = document.getElementById("nav-bar");
+    if (nav.style.display === "block") {
+        nav.style.display = "none";
+    }
+    else {
+        nav.style.display = "block";
+    }
+}
+
+function userAccount_desktop() {
+
+
+
+}
